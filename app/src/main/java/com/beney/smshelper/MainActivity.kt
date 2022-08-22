c99eec26744afbd3d221b0f2a51057f3dce729cd
beneychen <beneychen@microsoft.com>
2022-08-22 16:45:43 +0800

    configure dynamically

--- app/src/main/java/com/beney/smshelper/MainActivity.kt
+++ app/src/main/java/com/beney/smshelper/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.beney.smshelper
 
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var receiver: SMSReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        receiver = SMSReceiver()
+        var intentFilter = IntentFilter().apply {
+            addAction("android.provider.Telephony.SMS_RECEIVED")
+            priority = 10000
+        }
+        Toast.makeText(this, "INIT", Toast.LENGTH_SHORT).show()
+        registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
--- app/src/main/java/com/beney/smshelper/SMSReceiver.kt
+++ app/src/main/java/com/beney/smshelper/SMSReceiver.kt
@@ -7,6 +7,8 @@ import android.content.Intent
 import android.os.Build
 import android.telephony.SmsMessage
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class SMSReceiver : BroadcastReceiver() {
     companion object {
@@ -14,8 +16,10 @@ class SMSReceiver : BroadcastReceiver() {
         val PDU_TYPE = "pdus"
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onReceive(context: Context, intent: Intent) {
-        Log.d(TAG, "RECEIVE SMS")
+        Toast.makeText(context, "Receive SMS", Toast.LENGTH_SHORT).show()
+        Log.d(TAG, "[TEST]")
         val extra = intent.extras ?: return
         var pdus = extra.get(PDU_TYPE) as Array<ByteArray>
         val format = extra.getString("format")
